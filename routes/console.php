c50f3c23cd15e6607cc1c221b164fefc3cc3aece
manish98625 <sraj09231@gmail.com>
2026-05-21 11:03:06 +0545

    corn job

--- routes/console.php
+++ routes/console.php
@@ -13,3 +13,9 @@ Schedule::command('queue:work --stop-when-empty --tries=3')
     ->everyMinute()
     ->withoutOverlapping()
     ->runInBackground();
+
+// Clear and rebuild caches daily at 3 AM
+Schedule::command('optimize:clear')
+    ->dailyAt('03:00')
+    ->withoutOverlapping()
+    ->runInBackground();
