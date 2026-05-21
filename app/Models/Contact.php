839b1b48c3d053f134bf6fe28b093482c789f988
manish98625 <sraj09231@gmail.com>
2026-05-21 12:02:35 +0545

    fix

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Contact extends Model
 {
+
+    use CrudTrait;
+    use HasFactory;
     protected $fillable = [
         'name',
         'email',
